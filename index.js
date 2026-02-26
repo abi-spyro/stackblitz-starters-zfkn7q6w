ea158d61548e6f10d3c08b5bf4bdad97682d2cda
Aleksandra Bielak <103241694+abi-spyro@users.noreply.github.com>
2026-02-26 15:29:01 +0100

    Change Referer header to use req.hostname

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/api/tripadvisor/*', async (req, res) => {
         },
         headers: {
           accept: "application/json",
-          Referer: req.host,
+          Referer: req.hostname,
         },
         timeout: 10000 // 10s
       }
